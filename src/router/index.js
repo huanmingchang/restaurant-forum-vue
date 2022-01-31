668be7696e8b5c8adbd34fc38d4e195c265fdf4e
huanmingchang <huanming1988@gmail.com>
2022-01-31 12:37:18 +0800

    feat: add AdminRestaurantForm and AdminRestaurantNew components

--- src/router/index.js
+++ src/router/index.js
@@ -63,6 +63,11 @@ const routes = [
     component: () => import('../views/AdminRestaurants.vue'),
   },
   {
+    path: '/admin/restaurants/new',
+    name: 'admin-restaurant-new',
+    component: () => import('../views/AdminRestaurantNew.vue'),
+  },
+  {
     path: '/admin/restaurants/:id',
     name: 'admin-restaurant-show',
     component: () => import('../views/AdminRestaurantShow.vue'),
