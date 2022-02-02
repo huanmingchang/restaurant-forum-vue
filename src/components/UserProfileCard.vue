459d85b609fa46382cb7feb9036dba29e216c86e
huanmingchang <huanming1988@gmail.com>
2022-02-02 11:06:37 +0800

    feat: add User route and add UserProfileCard component

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="card mb-3">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="userProfile.image" width="300px" height="300px" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ userProfile.name }}</h5>
+          <p class="card-text">{{ userProfile.email }}</p>
+          <ul class="list-unstyled list-inline">
+            <li>
+              <strong> {{ userProfile.Comments.length }}</strong> 已評論餐廳
+            </li>
+            <li>
+              <strong> {{ userProfile.FavoritedRestaurants.length }}</strong>
+              收藏的餐廳
+            </li>
+            <li>
+              <strong> {{ userProfile.Followings.length }}</strong> followings
+              (追蹤者)
+            </li>
+            <li>
+              <strong> {{ userProfile.Followers.length }}</strong> followers
+              (追隨者)
+            </li>
+          </ul>
+          <p>
+            <router-link to="" v-if="userProfile.isAdmin"
+              ><button type="submit" class="btn btn-primary mr-2">
+                edit
+              </button></router-link
+            >
+            <template v-else>
+              <button
+                v-if="!userProfile.isFollowed"
+                type="submit"
+                class="btn btn-primary"
+                @click.stop.prevent="addFollowing"
+              >
+                追蹤
+              </button>
+              <button
+                v-else
+                type="submit"
+                class="btn btn-danger"
+                @click.stop.prevent="deleteFollowing"
+              >
+                取消追蹤
+              </button>
+            </template>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfileCard',
+  props: {
+    initialUserProfile: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      userProfile: {},
+    }
+  },
+  methods: {
+    fetchUserProfile() {
+      this.userProfile = this.initialUserProfile
+    },
+    addFollowing() {
+      this.userProfile = {
+        ...this.userProfile,
+        isFollowed: true,
+      }
+    },
+    deleteFollowing() {
+      this.userProfile = {
+        ...this.userProfile,
+        isFollowed: false,
+      }
+    },
+  },
+  created() {
+    this.fetchUserProfile()
+  },
+}
+</script>
