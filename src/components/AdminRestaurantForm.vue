35db73680195b0850d9b2d7635dd84ca24a04b33
huanmingchang <huanming1988@gmail.com>
2022-02-02 09:00:39 +0800

    feat: add upload image function

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -81,12 +81,20 @@
 
     <div class="form-group">
       <label for="image">Image</label>
+      <img
+        v-if="restaurant.image"
+        :src="restaurant.image"
+        class="d-block img-thumbnail mb-3"
+        width="200"
+        height="200"
+      />
       <input
         id="image"
         type="file"
         name="image"
         accept="image/*"
         class="form-control-file"
+        @change="handleFileChange"
       />
     </div>
 
@@ -144,6 +152,18 @@ export default {
   methods: {
     fetchCategories() {
       this.categories = dummyData.categories
+    },
+    handleFileChange(e) {
+      const { files } = e.target
+
+      if (files.length === 0) {
+        // 長度為 0 表示使用者沒有上傳資料
+        this.restaurant.image = ''
+        return
+      } else {
+        const imageURL = window.URL.createObjectURL(files[0])
+        this.restaurant.image = imageURL
+      }
     },
   },
   created() {
