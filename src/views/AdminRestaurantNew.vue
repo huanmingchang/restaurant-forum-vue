9b07fe0163680dae37013baa90a108993cb6856e
huanmingchang <huanming1988@gmail.com>
2022-02-02 09:24:27 +0800

    feat: add $emit event pass formData to AdminRestaurantNew

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
     <!-- 餐廳表單 AdminRestaurantForm -->
-    <AdminRestaurantForm />
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
   </div>
 </template>
 
@@ -12,6 +12,14 @@ export default {
   name: 'AdminRestaurantNew',
   components: {
     AdminRestaurantForm,
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // TODO 透過 API 向後端新增餐廳資料
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ':' + value)
+      }
+    },
   },
 }
 </script>
