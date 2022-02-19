f9a4c15e8f81c7ddb6321f80c857eb49d951aac3
huanmingchang <huanming1988@gmail.com>
2022-02-19 09:51:42 +0800

    feat: update signIn page to get vuex state and pass to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -46,45 +46,12 @@
 </template>
 
 <script>
-// 模擬 API 回傳資料
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: '管理者',
-    email: 'root@example.com',
-    image: 'https://i.pravatar.cc/300',
-    isAdmin: true,
-  },
-  isAuthenticated: true,
-}
+import { mapState } from 'vuex'
 
 export default {
   name: 'Navbar',
-  data() {
-    return {
-      // 預設資料
-      currentUser: {
-        id: -1,
-        name: '',
-        email: '',
-        image: '',
-        isAdmin: false,
-      },
-      isAuthenticated: false,
-    }
-  },
-  methods: {
-    fetchUser() {
-      this.currentUser = {
-        // 先帶入預設值，再用拉取到的資料覆蓋
-        ...this.currentUser,
-        ...dummyUser.currentUser,
-      }
-      this.isAuthenticated = dummyUser.isAuthenticated
-    },
-  },
-  created() {
-    this.fetchUser()
+  computed: {
+    ...mapState(['currentUser', 'isAuthenticated']),
   },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: {
+      id: -1,
+      name: '',
+      email: '',
+      image: '',
+      isAdmin: false,
+    },
+    isAuthenticated: false,
   },
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        ...currentUser,
+      }
+
+      state.isAuthenticated = true
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {},
 })
