c7b4515231e52711ec212eb7fcdd5ec30a652032
huanmingchang <huanming1988@gmail.com>
2022-02-19 11:38:32 +0800

    feat: add restaurantDashboard get restaurant api

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -21,89 +21,9 @@
 </template>
 
 <script>
-// 模擬 API 回傳的資料
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: 'Kale Herzog',
-    tel: '402.373.6546',
-    address: '666 Lukas Bridge',
-    opening_hours: '08:00',
-    description:
-      'Deleniti magni incidunt culpa veniam nihil aperiam architecto sed eveniet. Dolore veritatis quos ut aut sit eaque voluptatibus velit. Sunt sed autem sapiente quas harum consequatur consequatur. Pariatur qui quibusdam impedit incidunt porro voluptatum nulla culpa. Inventore perspiciatis dolores est. Ut facere blanditiis dolor iste animi eos.',
-    image:
-      'https://loremflickr.com/320/240/restaurant,food/?random=19.598578498989916',
-    viewCounts: 1,
-    createdAt: '2022-01-26T13:54:41.000Z',
-    updatedAt: '2022-01-29T02:13:10.000Z',
-    CategoryId: 5,
-    Category: {
-      id: 5,
-      name: '素食料理',
-      createdAt: '2022-01-26T13:54:41.000Z',
-      updatedAt: '2022-01-26T13:54:41.000Z',
-    },
-    Comments: [
-      {
-        id: 1,
-        text: 'Dolorem aliquam ea voluptates impedit eos.',
-        UserId: 3,
-        RestaurantId: 1,
-        createdAt: '2022-01-26T13:54:41.000Z',
-        updatedAt: '2022-01-26T13:54:41.000Z',
-        User: {
-          id: 3,
-          name: 'user2',
-          email: 'user2@example.com',
-          password:
-            '$2a$10$4Ie9Vnx3hAyT3m7OvhXaAeYxMjXDlLFUKScopJA8Tv1TyK9HS2aV.',
-          isAdmin: false,
-          image: null,
-          createdAt: '2022-01-26T13:54:41.000Z',
-          updatedAt: '2022-01-26T13:54:41.000Z',
-        },
-      },
-      {
-        id: 51,
-        text: 'Maxime repudiandae voluptatem quia.',
-        UserId: 3,
-        RestaurantId: 1,
-        createdAt: '2022-01-26T13:54:41.000Z',
-        updatedAt: '2022-01-26T13:54:41.000Z',
-        User: {
-          id: 3,
-          name: 'user2',
-          email: 'user2@example.com',
-          password:
-            '$2a$10$4Ie9Vnx3hAyT3m7OvhXaAeYxMjXDlLFUKScopJA8Tv1TyK9HS2aV.',
-          isAdmin: false,
-          image: null,
-          createdAt: '2022-01-26T13:54:41.000Z',
-          updatedAt: '2022-01-26T13:54:41.000Z',
-        },
-      },
-      {
-        id: 101,
-        text: 'Illum ut officia maiores.',
-        UserId: 3,
-        RestaurantId: 1,
-        createdAt: '2022-01-26T13:54:41.000Z',
-        updatedAt: '2022-01-26T13:54:41.000Z',
-        User: {
-          id: 3,
-          name: 'user2',
-          email: 'user2@example.com',
-          password:
-            '$2a$10$4Ie9Vnx3hAyT3m7OvhXaAeYxMjXDlLFUKScopJA8Tv1TyK9HS2aV.',
-          isAdmin: false,
-          image: null,
-          createdAt: '2022-01-26T13:54:41.000Z',
-          updatedAt: '2022-01-26T13:54:41.000Z',
-        },
-      },
-    ],
-  },
-}
+import restaurantsAPI from './../apis/restaurants'
+import { Toast } from './../utils/helpers'
+
 export default {
   name: 'RestaurantDashboard',
   data() {
@@ -118,16 +38,31 @@ export default {
     }
   },
   methods: {
-    fetchRestaurant() {
-      this.restaurant.id = dummyData.restaurant.id
-      this.restaurant.name = dummyData.restaurant.name
-      this.restaurant.categoryName = dummyData.restaurant.Category.name
-      this.restaurant.commentsLength = dummyData.restaurant.Comments.length
-      this.restaurant.viewCounts = dummyData.restaurant.viewCounts
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await restaurantsAPI.getRestaurantShow({
+          restaurantId,
+        })
+        console.log(data)
+        this.restaurant.id = data.restaurant.id
+        this.restaurant.name = data.restaurant.name
+        this.restaurant.categoryName = data.restaurant.Category
+          ? data.restaurant.Category.name
+          : '未分類'
+        this.restaurant.commentsLength = data.restaurant.Comments.length
+        this.restaurant.viewCounts = data.restaurant.viewCounts
+      } catch (error) {
+        console.log(error)
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試',
+        })
+      }
     },
   },
   created() {
-    this.fetchRestaurant()
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
   },
 }
 </script>
